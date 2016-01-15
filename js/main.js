016d1f978dfe960d4491cb28608e49f85fa6b1d1
Sergey Petrov <petrov-84@mail.ru>
2016-01-15 16:02:10 +0300

    загрузка файла

--- js/main.js
+++ js/main.js
@@ -41,23 +41,47 @@ var Setwatermark = (function(){
 })();
 
 var Saveimg = (function(){
+
 	var init = function(){
 		_setUplisteners();
 	},
 		_setUplisteners = function(){
-			$('.save-btn').on('click', _saveFile)
+			$('.img_input').on('change', _saveFile)
 		},
+
 		_saveFile = function(event){
 				event.preventDefault();
+		if (!this.files[0]) {
+            console.log("Не выбран файл для загрузки");
+            return;
+        };
+
+			var inptfile = this,
+				imgID = this.dataset['img'], // id изображения
+				fakeinputID = this.dataset['fakeinput'], //fakeinput
+				fd = new FormData;
+
+				
+			fd.append(0, this.files[0]);
+			//fd.append('img', $inptfile.prop('files')[0]);
 
 				$.ajax({
 					url:"../php/uploadimg.php",
 					type:"POST",
-					data:{
-
-						
-					}
+					dataType: 'json',
+					data:fd,
+					processData: false, // не обрабатывать файлы
+        			contentType: false // не отправлять стрингреквест
 				})
+				.done(function(answer){
+					console.log(answer);
+					console.log("выполнено");
+				})
+				.fail(function(answer){
+
+					console.log(answer);
+					console.log("ошибка");
+				});
 
 
 		};
--- php/uploadimg.php
+++ php/uploadimg.php
@@ -1,30 +1,40 @@
-<? php
+<?php
 //Загрузка изображений на сервер, валидация.
 
+session_start();
+$path_folder = "img/";  // путь к файлу
 
-$path_folder = $_POST['uploadDir'];  // путь к файлу
-$maxVal = $_POST['maxSize'];       // размер файла
-$maxWidth = $_POST['maxWidth'];     // ширина
-$maxHeight = $_POST['maxHeight'];   // высота
 $filename = basename($_FILES[0]["name"]); // имя файла
-$session_dir = session_id() . "/"; // уникализированное имя директории для файлов юзера
+//$id_session = session_id() . "/"; // уникализированное имя директории для файлов юзера
 $tmp_file = $_FILES[0]["tmp_name"];  // временный файл
-$path_file = "../" . $path_folder . $session_dir . $filename; // путь и имя будущего файла
-$path_url = $target_dir . $session_dir . $filename;          // url к будущему файлу
+
+$path_file = $path_folder . $filename; // путь и имя будущего файла
+$path_url = $path_folder . $filename;          // url к будущему файлу
 
 
-if (!file_exists("../" . $target_dir . $session_dir)) {
-    mkdir("../" . $target_dir . $session_dir);
-}
-
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 $answer =  array();  // ответ от сервера
-        $answer['status'] = 'OK';
+
+if(!is_uploaded_file($tmp_file)){
+	$answer['text']	= "Нет файла";
+}
+else{
+	$answer['text']	= "Есть файл";
+}
+
+//$imageFileType = pathinfo($path_file, PATHINFO_EXTENSION;
+/*if(!move_uploaded_file($tmp_file, $path_file)){
+	 	$answer['status'] = 'error';
+}
+else{
+	   $answer['status'] = 'OK';
         
+}*/
 
+		$answer['filename'] = $filename;
+        $answer['tmpfile'] = $tmp_file;
+        $answer['path'] = $path_file;
 
+//$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 echo json_encode($answer);
-
-
 ?>
